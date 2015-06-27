6f7c06d9ab6ea612cbaa2cd09ef3e5d43ea37902
Bjarke Christensen <mail@bjarkechr.dk>
2015-06-27 23:03:22 +0200

    A bit more work on Add raid

--- app/components/administration/raidAdminController.js
+++ app/components/administration/raidAdminController.js
@@ -12,29 +12,14 @@
         vm.plannedRaids = [];
         vm.finishedRaids = [];
         vm.instances = [];
-
+        vm.selectedInstance;
         vm.addRaidStartDate = new Date();
-        vm.addRaidStartTime = '';
-        vm.hours = [];
-        vm.minutes = [];
-        vm.addRaid = addRaid;
-        vm.open = openDatePicker;
-
-        for (var i = 0; i < 24; i++) {
-            vm.hours.push({
-                value: i,
-                text: (i < 10) ? '0' + i.toString() : i.toString()
-            });
-        }
-
-        for (var i = 0; i < 60; i++) {
-            vm.minutes.push({
-                value: i,
-                text: (i < 10) ? '0' + i.toString() : i.toString()
-            });
-        }
+        vm.hours = generateHourMinuteArray(24);
+        vm.minutes = generateHourMinuteArray(60);
         vm.startHour = vm.hours[20];
         vm.startMinute = vm.minutes[0];
+        vm.addRaid = addRaid;
+        vm.open = openDatePicker;
 
         raidFactory.query(null, raidQueryResult);
         instanceFactory.query(null, instanceQueryResult);
@@ -43,8 +28,8 @@
             var raids = data;
             var len = raids.length;
             for (var i = 0; i < len; i++) {
-                
-                 $log.log(raids[i].status);
+
+                $log.log(raids[i].status);
                 if (raids[i].status == "Planned") {
                     vm.plannedRaids.push(raids[i]);
                 }
@@ -59,15 +44,25 @@
 
         function instanceQueryResult(data) {
             vm.instances = data;
+            
+            vm.selectedInstance = vm.instances[0];
         }
 
         function addRaid() {
-            $log.log("submit!");
 
             var newRaid = new raidFactory();
-            newRaid.id = "100";
-            newRaid.start = "2015-06-24 20:00:00";
-            newRaid.instance = "Blackrock Foundry";
+
+            var day = vm.addRaidStartDate.getDate();
+            day = (day < 10) ? '0' + day.toString() : day.toString();
+            var month = vm.addRaidStartDate.getMonth() + 1;
+            month = (month < 10) ? '0' + month.toString() : month.toString();
+
+            var dateString = vm.addRaidStartDate.getFullYear() + "-" + month + "-" + day;
+            dateString = dateString + " " + vm.startHour + ":" + vm.startMinute + ":00";
+
+            newRaid.start = dateString;            
+            newRaid.instance = vm.selectedInstance.id;
+            
             newRaid.$save();
         }
 
@@ -76,6 +71,16 @@
             $event.stopPropagation();
             vm.datePickerOpened = true;
         };
+
+        function generateHourMinuteArray(max) {
+            var result = [];
+            for (var i = 0; i < max; i++) {
+                result.push(
+                    (i < 10) ? '0' + i.toString() : i.toString()
+                );
+            }
+            return result;
+        }
     }
 
 })();
