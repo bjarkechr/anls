612eae8c7f34751e4b7ce56c94b98c01da45c6e3
Bjarke Christensen <mail@bjarkechr.dk>
2015-07-21 23:44:49 +0200

    Added method getNoContext to blizzardItemFactory

--- app/shared/resourceFactories/blizzardItemFactory.js
+++ app/shared/resourceFactories/blizzardItemFactory.js
@@ -9,8 +9,12 @@
         return $resource('https://eu.api.battle.net/wow/item/:itemId/:context?locale=en_GB&apikey=y3sj5tk7grmdrdqgcday53ahwsmpvycm', {
             itemId: '@id',
             context: '@context'
+        }, {
+            'getNoContext': {
+                method: 'GET',
+                url: 'https://eu.api.battle.net/wow/item/:itemId?locale=en_GB&apikey=y3sj5tk7grmdrdqgcday53ahwsmpvycm'
+            }
         });
-
     }
 
 })();
